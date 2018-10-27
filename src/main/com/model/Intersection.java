9047016e0caa7978b7ebe22dca2f6567bc039e69
kohlkuntze <kohlkuntze@gmail.com>
2018-10-27 11:51:19 -0400

    Initial commitn intersection

---
+++ src/main/com/model/Intersection.java
@@ -0,0 +1,22 @@
+package model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NonNull;
+
+import java.util.List;
+import java.util.Queue;
+
+@Builder
+@Getter
+public class Intersection {
+
+    @NonNull
+    private final List<Queue> lanes;
+
+    @NonNull
+    private final List<Double> times;
+
+    @NonNull
+    private final List<List<Integer>> laneOrder;
+}
